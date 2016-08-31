d1867be3f94ef013093c7bb5617f48fb94566463
FG Ribreau <github@fgribreau.com>
2016-08-31 11:35:33 +0200

    fix(test)

--- index.test.js
+++ index.test.js
@@ -4,13 +4,28 @@ const t = require('chai').assert;
 const assert = require('assert');
 
 describe('smtp', function() {
-  it('should send an email', function(f) {
-    assert(process.env.SMTP_HOST, 'process.env.SMTP_HOST environment variable is missing');
-    assert(process.env.SMTP_PORT, 'process.env.SMTP_PORT environment variable is missing');
-    assert(process.env.SMTP_USER, 'process.env.SMTP_USER environment variable is missing');
-    assert(process.env.SMTP_PASS, 'process.env.SMTP_PASS environment variable is missing');
+  assert(process.env.SMTP_HOST, 'process.env.SMTP_HOST environment variable is missing');
+  assert(process.env.SMTP_PORT, 'process.env.SMTP_PORT environment variable is missing');
+  assert(process.env.SMTP_USER, 'process.env.SMTP_USER environment variable is missing');
+  assert(process.env.SMTP_PASS, 'process.env.SMTP_PASS environment variable is missing');
 
-    // require('./').MailProvider.DEBUG_MODE=true;
+  it('should throw an error if from is bad defined', function() {
+    t.throws(function(){
+      provider = require('./')({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: true,
+        debug: true,
+        auth:{
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        }
+      });
+    });
+  });
+
+
+  it('should send an email', function(f) {
     const provider = require('./')({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
@@ -20,12 +35,13 @@ describe('smtp', function() {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       }
+    }, {
+      from: 'Sender Name <transacemail-sendgrid-test@fgribreau.com>'
     });
 
     provider.send({
       html: '<strong>hello :)</strong>',
       text: 'hello :)',
-      from: 'transacemail-sendgrid-test@fgribreau.com',
       data: {
         email: 'transacemail-sendgrid@fgribreau.com',
         name: 'Transacemail Sendgrid'
